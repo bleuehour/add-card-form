19c46b5926c0389aa7033fbbc8e00894aa2d7c32
theodeeney <theo.paraskeva123@gmail.com>
2023-12-25 01:49:49 +0000

    final css changes

--- main.js
+++ main.js
@@ -1,66 +1,133 @@
-/// Card number on card
-const numberSpanTags = document.querySelectorAll(".card-number span");
-let currentNumber = "";
-/// Card number input field
-const cardNumberField = document.getElementById("cardNumber");
+const focus = document.querySelector(".card-focus");
 
-cardNumberField.addEventListener("input", (e) => {
-  const match = /^[0-9]*$/;
+function updateCardNumber() {
+  /// Card number on card
+  const numberSpanTags = document.querySelectorAll(".card-number span");
+  let currentNumber = "";
+  const cardNumberField = document.getElementById("cardNumber");
 
-  if (!match.test(e.target.value)) {
-    return;
-  }
-  const index = e.target.value.length - 1;
-  if (e.data === null) {
-    if (Math.abs(currentNumber.length - e.target.value.length) === 1) {
-      numberSpanTags[index + 1].innerHTML = `<span>#<br/></span>`;
-      numberSpanTags[index + 1].classList.remove("typed");
-    } else {
-      let i = e.target.value.length;
-      while (i < 16) {
-        numberSpanTags[i].innerHTML = `<span>#<br/></span>`;
-        numberSpanTags[i].classList.remove("typed");
-        i += 1;
-      }
+  /// Card number input field
+
+  cardNumberField.addEventListener("focusin", () => {
+    const cardNumber = document.querySelector(".card-number");
+    focus.style.width = `${cardNumber.clientWidth - 97}px`;
+    focus.style.height = "25px";
+    focus.style.top = "53%";
+    focus.style.left = "8%";
+    focus.style.opacity = "1";
+  });
+
+  cardNumberField.addEventListener("focusout", () => {
+    focus.style.opacity = "0";
+  });
+
+  cardNumberField.addEventListener("input", (e) => {
+    const match = /^[0-9]*$/;
+
+    if (!match.test(e.target.value)) {
+      return;
     }
-  } else {
-    numberSpanTags[index].innerHTML = `<span>#<br/>${e.data}</span>`;
-    numberSpanTags[index].classList.add("typed");
-    currentNumber = e.target.value;
-  }
-});
+    const index = e.target.value.length - 1;
+    if (e.data === null) {
+      if (Math.abs(currentNumber.length - e.target.value.length) === 1) {
+        numberSpanTags[index + 1].innerHTML = `<span>#<br/></span>`;
+        numberSpanTags[index + 1].classList.remove("typed");
+      } else {
+        let i = e.target.value.length;
+        while (i < 16) {
+          numberSpanTags[i].innerHTML = `<span>#<br/></span>`;
+          numberSpanTags[i].classList.remove("typed");
+          i += 1;
+        }
+      }
+    } else {
+      numberSpanTags[index].innerHTML = `<span>#<br/>${e.data}</span>`;
+      numberSpanTags[index].classList.add("typed");
+      currentNumber = e.target.value;
+    }
+  });
+}
 
-/// CARD DATES
-const cardDateTag = document.querySelector(".card-date");
-const monthSelect = document.getElementById("expired-month");
-const yearSelect = document.getElementById("expired-year");
+function updateDates() {
+  /// CARD DATES
+  const cardDateTag = document.querySelector(".card-date");
+  const monthSelect = document.getElementById("expired-month");
+  const yearSelect = document.getElementById("expired-year");
 
-let month = "01";
-let year = "23";
+  let month = "01";
+  let year = "23";
 
-/// MONTH CHANGE
-monthSelect.addEventListener("change", (e) => {
-  month = e.target.value;
-  cardDateTag.innerHTML = `<span>exp end </span> ${month}/${year}`;
-});
-/// YEAR CHANGE
-yearSelect.addEventListener("change", (e) => {
-  year = e.target.value;
-  cardDateTag.innerHTML = `<span>exp end </span> ${month}/${year}`;
-});
+  monthSelect.addEventListener("focusin", () => {
+    const cardDate = document.querySelector(".card-date");
+    focus.style.width = `${cardDate.clientWidth - 282}px`;
+    focus.style.height = "25px";
+    focus.style.top = "71%";
+    focus.style.left = "40%";
+    focus.style.opacity = "1";
+  });
+  monthSelect.addEventListener("focusout", () => {
+    focus.style.opacity = "0";
+  });
+  yearSelect.addEventListener("focusin", () => {
+    const cardDate = document.querySelector(".card-date");
+    focus.style.width = `${cardDate.clientWidth - 282}px`;
+    focus.style.height = "25px";
+    focus.style.top = "71%";
+    focus.style.left = "40%";
+    focus.style.opacity = "1";
+  });
+  yearSelect.addEventListener("focusout", () => {
+    focus.style.opacity = "0";
+  });
 
-/// CARD NAMES
-const cardNameTag = document.querySelector(".card-name");
-const nameInputField = document.getElementById("cardName");
+  /// MONTH CHANGE
+  monthSelect.addEventListener("change", (e) => {
+    month = e.target.value;
+    cardDateTag.innerHTML = `<span>exp end </span> ${month}/${year}`;
+  });
+  /// YEAR CHANGE
+  yearSelect.addEventListener("change", (e) => {
+    year = e.target.value;
+    cardDateTag.innerHTML = `<span>exp end </span> ${month}/${year}`;
+  });
+}
 
-nameInputField.addEventListener("input", (e) => {
-  const default_name = "Monzo Member";
+function updateNames() {
+  /// CARD NAMES
+  const cardNameTag = document.querySelector(".card-name");
+  const nameInputField = document.getElementById("cardName");
 
-  if (e.target.value.length === 0) {
-    cardNameTag.innerText = default_name;
-  } else {
-    cardNameTag.innerText = e.target.value;
-  }
+  nameInputField.addEventListener("focusin", () => {
+    const cardName = document.querySelector(".card-name");
+    focus.style.width = `${cardName.clientWidth}px`;
+    focus.style.height = "25px";
+    focus.style.top = "85%";
+    focus.style.left = "5%";
+    focus.style.opacity = "1";
+  });
+  nameInputField.addEventListener("focusout", () => {
+    focus.style.opacity = "0";
+  });
+
+  nameInputField.addEventListener("input", (e) => {
+    const default_name = "Monzo Member";
+
+    if (e.target.value.length === 0) {
+      cardNameTag.innerText = default_name;
+    } else {
+      cardNameTag.innerText = e.target.value;
+    }
+
+    const cardName = document.querySelector(".card-name");
+
+    focus.style.width = `${cardName.clientWidth}px`;
+  });
+}
+
+window.addEventListener("load", () => {
+  updateCardNumber();
+  updateDates();
+  updateNames();
 });
 
 //// Tilt
